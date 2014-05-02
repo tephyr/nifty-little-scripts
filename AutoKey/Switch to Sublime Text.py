4f5ec2a3bc6811e90a43c4ca5fa29b6ca743ed0a
Andrew Ittner <aji@rhymingpanda.com>
2014-05-02 09:49:36 -0700

    Works!
    - launches sublime_text if not found in window list
    - activates first available instance, if not in front
    - switches between instances

--- AutoKey/Switch to Sublime Text.py
+++ AutoKey/Switch to Sublime Text.py
@@ -1,45 +1,41 @@
 import subprocess
 
-# get current window class
-winClass = window.get_active_class()
+# get all current windows
+command = 'wmctrl -l'
+output = system.exec_command(command, getOutput=True)
+stlines = []
+for line in output.split("\n"):
+    if "Sublime Text" in line:
+        stlines.append(line)
 
-# if current window class is not "sublime_text.Sublime_text", then activate "Sublime"
-if False: #"sublime_text" not in winClass:
-    window.activate("Sublime", switchDesktop=True)
+#dialog.info_dialog(title="stlines", message="stlines.count: {0}".format(len(stlines)))
+if len(stlines) == 0:
+    # sublime text not running
+    subprocess.Popen(["sublime_text"])
 else:
-    # if it is, then search for other windows (wmctrl -l)
-    command = 'wmctrl -l'
-    output = system.exec_command(command, getOutput=True)
-    stlines = []
-    for line in output.split("\n"):
-        if "Sublime Text" in line:
-            stlines.append(line)
+    # an instance of Sublime Text is running
+    winClass = window.get_active_class()
 
-    #dialog.info_dialog(title="stlines", message="stlines.count: {0}".format(len(stlines)))
-    if len(stlines) == 0:
-        # sublime text not running
-        #result = dialog.input_dialog(title="Sublime Text not running", message="Launch Sublime Text now?")
-        #dialog.info_dialog(title="?", message="Result: {0}".format(result))
-        #subprocess.Popen(["/usr/bin/firefox"])
-        subprocess.Popen(["sublime_text"])
-    elif len(stlines) == 1:
+    if "sublime_text" not in winClass:
+        # if current window class is not "sublime_text.Sublime_text", then activate "Sublime"
+        # doesn't matter how many instances are running; just activate one
         window.activate("Sublime", switchDesktop=True)
-    elif len(stlines) > 1:
-        # multiple windows, move to next
+    elif "sublime_text" in winClass and len(stlines) > 1:
+        # multiple windows, Sublime Text already active, move to next
         current_title = window.get_active_title()
         stinfolines = []
         # strip first 3 columns of each line 
-        # beware: the spaces may be multiple
+        # beware: may be multiple spaces
         for line in stlines:
             # take out multiple spaces in 1st 16 characters
             if "  " in line[:16]:
                 line = line.replace("  ", " ", 1)
             stinfolines.append(line.split(" ", 3)[3])
         
-        dialog.info_dialog("Sublime Text window info",
-            "current window: {0}\nother windows\n{1}".format(window.get_active_title(),
-                "\n".join(stinfolines)
-            ))
+        #dialog.info_dialog("Sublime Text window info",
+        #    "current window: {0}\nother windows\n{1}".format(window.get_active_title(),
+        #        "\n".join(stinfolines)
+        #    ))
 
         # find *next* line to activate
         if stinfolines[-1] == current_title:
